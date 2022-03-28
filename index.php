50308d2c664e761fb9a5c6b7414c2e66335757c4
AndyChiang0312 <89000322+AndyChiang0312@users.noreply.github.com>
2022-03-28 14:03:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>4D 2122</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-index.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="navbarcontent">
+            <p href="#">首頁</p>
+            <p href="#">網課連結</p>
+            <p href="#">功課</p>
+            <p href="#">生日</p>
+        </div>
+    </div>
+    <div class="section1">
+        <div class="wrapper1">
+            <div class="text">
+                <h1>網課連結與時間表<br>準備好了</h1>
+                <h3>按此進入></h3>
+            </div>
+        </div>
+        <div class="image">
+            <img src="media/indexIcon/Zoom.png">
+        </div>
+    </div>
+    <div class="section2">
+        <div class="wrapper1">
+            <div class="text">
+                <h1>永遠不會忘記<br>你的生日</h1>
+                <h3>查看生日></h3>
+            </div>
+        </div>
+        <div class="image">
+            <img src="media/indexIcon/birthday.png">
+        </div>
+    </div>
+    <!-- <div class="region1">
+        <h1>BONJOUR</h1>
+    </div>
+    <div class="main">
+        <div class="welcome">
+            <h1>WELCOME TO<br>4D</h1>
+        </div>
+        <div class="login">
+            <div class="loginbox">
+                <h1>LOGIN BOX</h1>
+                <input type="text"><br>
+                <input type="password"><br>
+                <input type="submit">
+            </div>
+        </div>
+    </div> -->
+</body>
+<footer>
+    <div class="footer">
+        <div class="wrapper">
+            <h1>已經到底了</h1>
+        </div>
+    </div>
+</footer>
+</html>
---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-login.css">
+</head>
+<body>
+    <div class="main">
+        <div class="wrapper">
+            <div class="login">
+                <div class="inner">
+                    <h1>4D</h1>
+                    <input type="text" name="username" placeholder="Username"><br>
+                    <input type="password" placeholder="Password"><br>
+                    <button class="submit">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
